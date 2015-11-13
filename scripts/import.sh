a10e3e3bfdfbd8f2346bad07317974fa4c78d1d9
Dmitry Schegolihin <d.schegolihin@kmplzt.de>
2015-11-13 13:43:45 +0400

    Remove import.sh. Remove DATABASE_NAME.

--- scripts/import.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysql -uroot -ptmp -hmysql -e "create database ${DATABASE_NAME};"
-pv -cN source /var/www/magento/dump.sql.gz | gunzip -c | pv -cN mysql | mysql -uroot -ptmp -hmysql ${DATABASE_NAME}
-mysql -uroot -ptmp -hmysql ${DATABASE_NAME} -e "update core_config_data set value='${BASE_URL}' where path like '%secure/base_url';"
